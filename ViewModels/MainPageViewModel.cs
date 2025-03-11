d4e1601397aac9f377752416a9cf5eab50a275f5
Fredrik von Post <fredrik.v.post@gmail.com>
2025-03-11 15:09:53 +0100

    Add Community Toolkit implementations

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,25 +1,15 @@
 ﻿using CalculusApp.Services;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CalculusApp.ViewModels;
 
-public class MainPageViewModel : INotifyPropertyChanged
+public partial class MainPageViewModel : ObservableObject
 {
     public int MyProperty { get; set; }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
-
+    [ObservableProperty] 
     private string _solution = "";
-    public string Solution
-    {
-        get => _solution;
-        set
-        {
-            _solution = value;
-            OnPropertyChanged();
-        }
-    }
 
     private readonly SolutionService _solutionService;
 
@@ -28,12 +18,14 @@ public class MainPageViewModel : INotifyPropertyChanged
         _solutionService = solutionService;
     }
 
+    [RelayCommand]
+    private async Task NewtonClicked()
+    {
+        Solution = await GetSolutionAsync();
+    }
+
     public async Task<string> GetSolutionAsync()
     {
         return await _solutionService.GetSolutionAsync("derive","x^2");
-    }
-    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -4,17 +4,9 @@ namespace CalculusApp;
 
 public partial class MainPage : ContentPage
 {
-    private readonly MainPageViewModel _mainPageViewModel;
-
     public MainPage(MainPageViewModel mainPageViewModel)
     {
         InitializeComponent();
-        _mainPageViewModel = mainPageViewModel;
-        BindingContext = _mainPageViewModel;
-    }
-
-    private async void OnNewtonClicked(object sender, EventArgs e)
-    {
-        _mainPageViewModel.Solution = await _mainPageViewModel.GetSolutionAsync();
+        BindingContext = mainPageViewModel;
     }
 }
